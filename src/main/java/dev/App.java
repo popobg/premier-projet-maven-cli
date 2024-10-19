3e7834ca05a4579272bbf534b9ecd086e8828095
popobg <pauline.bouyssou1@gmail.com>
2024-10-19 21:22:46 +0200

    config plugins for Maven + small modif on the App class

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,6 +1,8 @@
 package dev;
 
 import com.github.lalyos.jfiglet.FigletFont;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
@@ -11,6 +13,11 @@ import java.io.IOException;
 public class App 
 {
     public static void main( String[] args ) {
+        Logger LOGGER = LoggerFactory.getLogger(App.class);
+
+        LOGGER.info("Hello Slf4J !");
+        LOGGER.info("Logback implementation");
+
         String asciiArt1 = "Hello World !";
 
         try {
@@ -20,6 +27,7 @@ public class App
             e.printStackTrace();
         }
 
+        System.out.println();
         System.out.println(asciiArt1);
     }
 }
